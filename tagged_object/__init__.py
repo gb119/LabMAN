1af42cea7be584026c720dc20f958b8876baf122
Gavin Burnell <g.burnell@leeds.ac.uk>
2016-03-31 22:05:27 +0100

    Removed no-longer user apps

--- tagged_object/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'tagged_object.apps.Tagged_ObjectAppConfig'
--- tagged_object/apps.py
+++
@@ -1,6 +0,0 @@
-# in yourapp/apps.py
-from django.apps import AppConfig
-
-class Tagged_ObjectAppConfig(AppConfig):
-    name = 'tagged_object'
-    verbose_name = 'Tagged Items'
